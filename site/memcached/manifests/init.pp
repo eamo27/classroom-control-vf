7a1197ae23a176be17066876ab36b20892b3ddb7
eamo27 <eamo27@yahoo.ie>
2016-04-27 15:42:01 +0100

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,16 @@
+class memcached {
+
+  package { 'memcached': 
+    ensure => present,
+    before => File['memcached'],
+  }
+  file { 'memcached': 
+    ensure => file,
+    path => '/etc/sysconfig/memcached',
+    source => 'puppet:///modules/memcached/memcached',
+    notify => Service['memcached'],
+  }
+  service { 'memcached': 
+    ensure => running,
+    enable => true,
+  }
