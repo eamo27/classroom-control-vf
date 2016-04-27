53fea741dac978c93779b347e5640df56520a687
eamo27 <eamo27@yahoo.ie>
2016-04-27 16:28:36 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,19 @@
+class nginx {
+  package { 'nginx': 
+    ensure => present,
+    before => File['nginx'],
+  }
+  file { 'nginx': 
+    ensure => file,
+    path => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    notify => Service['nginx'],
+  }
+  service { 'nginx': 
+    ensure => running,
+    enable => true,
+    }
+}
+  
+  
 
