745804834edbb34221245d384d5402d53fe9e37c
eamo27 <eamo27@yahoo.ie>
2016-04-28 09:42:13 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,14 +5,14 @@ class nginx {
   }
   file { 'nginx': 
     ensure => file,
-    path => '/etc/nginx/nginx.conf',
+    path => '/etc/nginx/conf.d/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
     notify => Service['nginx'],
   }
   
   file { 'nginx-default': 
     ensure => file,
-    path => '/etc/nginx/default.conf',
+    path => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
     notify => Service['nginx'],
   }
