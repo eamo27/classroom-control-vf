2c0e8e343494eef84cf669c68a9d4459150e79bc
eamo27 <eamo27@yahoo.ie>
2016-04-28 16:28:50 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -19,16 +19,7 @@ $logdir = '/var/log/nginx'
 # this will be used if we don't pass in a value
 $default_docroot = '/var/www'
 }
-'windows' : {
-$package = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-# $docroot = 'C:/ProgramData/nginx/html'
-$confdir = 'C:/ProgramData/nginx'
-$logdir = 'C:/ProgramData/nginx/logs'
-# this will be used if we don't pass in a value
-$default_docroot = 'C:/ProgramData/nginx/html'
-}
+
 default : {
 fail("Module ${module_name} is not supported on ${::osfamily}")
 }
