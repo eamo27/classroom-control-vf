acd123a7dd3533c20322632b96b1f4a7f583ad3e
eamo27 <eamo27@yahoo.ie>
2016-04-28 14:10:12 +0100

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,28 @@
+define users::managed_user (
+  $ensure = 'present',
+   manahehome = true,
+   $shell     = '/bin/bash',
+   $groups = undef,
+   $group = $title,
+   $home = "$/home/${title}",
+)
+{
+  user { $title:
+    ensure => $ensure,
+    manahehome => $managehome,
+    shell => $shell,
+    gid => $gid,
+    groups => $groups,
+    home => $home,
+    }
+    
+  if $ensure == 'present' {
+  group { $group:
+    ensure => present,
+    }
+  file { "${home}/.ssh:
+    ensure => directory,
+    owner => $title,
+  
+  }
+}
